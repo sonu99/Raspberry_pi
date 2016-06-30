d679f0a24ae7811739eba202fff6ef61e1dfc86f
sonu99 <sonu99@users.noreply.github.com>
2016-06-30 16:46:13 +0530

    Update python_Basic_doc.py

--- python_Basic_doc.py
+++ python_Basic_doc.py
@@ -1,3 +1,5 @@
 Python sample code
 http://www.hlevkin.com/Shell_progr/hellopython.htm
+for compilation use 
+http://www.tutorialspoint.com/execute_python_online.php
 #---------------------------------------------------------------------------------------------------------------------------------------
